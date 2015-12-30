15f9a87df702c87b3ce92cdf7b71041b005909a3
Caio Quinta <caiodaquinta@gmail.com>
2015-12-30 12:44:16 -0200

    not necessary to override devise method anymore

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,23 +1,3 @@
 # -*- encoding : utf-8 -*-
 class Users::RegistrationsController < Devise::RegistrationsController
-
-  # POST /resource
-  def create
-    super do |resource|
-        subPrefeitura = SubPrefeitura.find_by_codigo(params[:codigo])
-        
-        unless subPrefeitura.present?
-          flash[:alert] = "Código de subprefeitura inválido!"
-        else
-          if not subPrefeitura.limite_usuario_atingido
-            resource.sub_prefeitura = subPrefeitura
-            resource.save
-            PopMailer.termos_email(resource).deliver
-          else
-            flash[:alert] = "O limite de usuários dessa subprefeitura atingido!"
-          end
-        end
-    end
-  end
-
 end
